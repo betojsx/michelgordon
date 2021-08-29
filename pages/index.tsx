69f55716d6795ecbc0122629fe6fec5340a25ff3
Beto <betodasilvajr@gmail.com>
2021-08-28 22:27:32 -0300

    feat: adds banner initial structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,23 +6,64 @@ import Link from 'next/link';
 
 // Import the generated Lists API from Keystone
 import { lists } from '.keystone/api';
+import { Box } from '@chakra-ui/react';
+
+import dynamic from 'next/dynamic';
+
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const SLIDERS = [
+	{
+		imageSrc: 'https://source.unsplash.com/9MRX_KwALE0',
+	},
+	{
+		imageSrc: 'https://source.unsplash.com/bzz1SzUeu04',
+	},
+	{
+		imageSrc: 'https://source.unsplash.com/k-fMn9RKIXU',
+	},
+];
+//@ts-ignore
+const SliderItem = ({ imageSrc }) => (
+	<Box h="calc(100vh - 100px)">
+		<Image src={imageSrc} width={1600} height={1600} objectFit="cover" />
+	</Box>
+);
+
+const responsive = {
+	desktop: {
+		breakpoint: { max: 3000, min: 1024 },
+		items: 1,
+		slidesToSlide: 1, // optional, default to 1.
+	},
+	tablet: {
+		breakpoint: { max: 1024, min: 576 },
+		items: 1,
+		slidesToSlide: 1, // optional, default to 1.
+	},
+	mobile: {
+		breakpoint: { max: 576, min: 0 },
+		items: 1,
+		slidesToSlide: 1, // optional, default to 1.
+	},
+};
+const Slider = () => {
+	return (
+		<Carousel responsive={responsive}>
+			{SLIDERS.map((slideItem) => (
+				<SliderItem imageSrc={slideItem.imageSrc} />
+			))}
+		</Carousel>
+	);
+};
 
 // Home receives a `posts` prop from `getStaticProps` below
 export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
 	return (
 		<div>
-			<main style={{ margin: '3rem' }}>
-				<h1>Hello World! 👋🏻 </h1>
-				<ul>
-					{/* Render each post with a link to the content page */}
-					{posts?.map((post) => (
-						<li key={post.id}>
-							<Link href={`/post/${post.slug}`}>
-								<a>{post.title}</a>
-							</Link>
-						</li>
-					))}
-				</ul>
+			<main>
+				<Slider />
 			</main>
 		</div>
 	);
