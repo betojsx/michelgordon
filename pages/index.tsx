b1a643dc64679b8dd78a2247f482b321bd1c8226
Beto <betodasilvajr@gmail.com>
2021-08-28 23:35:17 -0300

    feat: add home carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 // Import the generated Lists API from Keystone
 import { lists } from '.keystone/api';
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Link as ChakraLink } from '@chakra-ui/react';
 
 import dynamic from 'next/dynamic';
 
@@ -26,7 +26,14 @@ const SLIDERS = [
 ];
 //@ts-ignore
 const SliderItem = ({ imageSrc }) => (
-	<Box h="calc(100vh - 100px)">
+	<Box
+		sx={{
+			'> *': {
+				h: '100%',
+			},
+		}}
+		h="calc(100vh - 100px)"
+	>
 		<Image src={imageSrc} width={1600} height={1600} objectFit="cover" />
 	</Box>
 );
@@ -48,23 +55,68 @@ const responsive = {
 		slidesToSlide: 1, // optional, default to 1.
 	},
 };
-const Slider = () => {
+//@ts-ignore
+const Slider = (props) => {
 	return (
-		<Carousel responsive={responsive}>
-			{SLIDERS.map((slideItem) => (
-				<SliderItem imageSrc={slideItem.imageSrc} />
-			))}
-		</Carousel>
+		<Box {...props}>
+			<Carousel responsive={responsive}>
+				{SLIDERS.map((slideItem, index) => (
+					<SliderItem imageSrc={slideItem.imageSrc} key={`si-${index}`} />
+				))}
+			</Carousel>
+		</Box>
 	);
 };
+
+const Menu = () => (
+	<Flex bg="rgba(26, 32, 44, 0.7)" px="8" justify="space-between" align="center" h={100} shadow="sm">
+		<Box w={16} h={16} bg="gray.300" borderRadius="full" />
+		<Flex as="nav">
+			<Box mr="4">
+				<Link href="#">
+					<ChakraLink>Home</ChakraLink>
+				</Link>
+			</Box>
+			<Box mr="4">
+				<Link href="#">
+					<ChakraLink>Obras</ChakraLink>
+				</Link>
+			</Box>
+			<Box mr="4">
+				<Link href="#">
+					<ChakraLink>Educação</ChakraLink>
+				</Link>
+			</Box>
+			<Box mr="4">
+				<Link href="#">
+					<ChakraLink>Sobre</ChakraLink>
+				</Link>
+			</Box>
+			<Box mr="4">
+				<Link href="#">
+					<ChakraLink>Contato</ChakraLink>
+				</Link>
+			</Box>
+			<Box>
+				<Link href="#">
+					<ChakraLink>Blog</ChakraLink>
+				</Link>
+			</Box>
+		</Flex>
+	</Flex>
+);
 
 // Home receives a `posts` prop from `getStaticProps` below
 export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
 	return (
 		<div>
-			<main>
-				<Slider />
-			</main>
+			<Box pt="calc(100vh - 100px)">
+				<Slider pos="fixed" top="0" left="0" right="0" />
+				<Box pos="relative" zIndex="1">
+					<Menu />
+					<Box h="800px" bg="gray.800"></Box>
+				</Box>
+			</Box>
 		</div>
 	);
 }
