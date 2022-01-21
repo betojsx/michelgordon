33c889d2240b82ac33f12370c0842345d226fd7b
Beto <betodasilvajr@gmail.com>
2022-01-21 15:20:58 -0300

    prevent token to be exposed publicly

--- utils/graphcmsClient.ts
+++ utils/graphcmsClient.ts
@@ -2,15 +2,29 @@ import { GraphQLClient } from 'graphql-request';
 
 export default () => {
 	let client: GraphQLClient;
-	if (process.env.NEXT_PUBLIC_GRAPHCMS_URL && process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN) {
-		const headers = {
-			Authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN}`,
-		};
-		client = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_URL, {
-			headers,
-		});
+	if (process.env.DEVELOPMENT) {
+		if (process.env.NEXT_PUBLIC_GRAPHCMS_URL && process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN) {
+			const headers = {
+				Authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN}`,
+			};
+			client = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_URL, {
+				headers,
+			});
+		} else {
+			throw new Error('Graphcms url missing');
+		}
 	} else {
-		throw new Error('Graphcms url missing');
+		if (process.env.NEXT_GRAPHCMS_URL && process.env.NEXT_GRAPHCMS_TOKEN) {
+			const headers = {
+				Authorization: `Bearer ${process.env.NEXT_GRAPHCMS_TOKEN}`,
+			};
+			client = new GraphQLClient(process.env.NEXT_GRAPHCMS_URL, {
+				headers,
+			});
+		} else {
+			throw new Error('Graphcms url missing');
+		}
 	}
+
 	return client;
 };
