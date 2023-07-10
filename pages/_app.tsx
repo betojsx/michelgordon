c6b36cd746e5926cbc3439981a193a6e9198cdec
betodasilva <jrs_robert@hotmail.com>
2023-07-10 13:19:05 -0300

    feat: add facebook domain verification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,34 @@
-import type { AppProps } from 'next/app';
-import { ChakraProvider, ColorModeProvider, CSSReset } from '@chakra-ui/react';
-import Head from 'next/head';
-import '@fontsource/poppins/200.css';
-import '@fontsource/poppins/300.css';
-import '@fontsource/poppins/400.css';
-import '@fontsource/poppins/700.css';
-import '@fontsource/poppins/600.css';
-import '@fontsource/rubik/400.css';
-import '@fontsource/rubik/700.css';
-import '@fontsource/rubik/600.css';
+import type { AppProps } from "next/app";
+import { ChakraProvider, ColorModeProvider, CSSReset } from "@chakra-ui/react";
+import Head from "next/head";
+import "@fontsource/poppins/200.css";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/rubik/400.css";
+import "@fontsource/rubik/700.css";
+import "@fontsource/rubik/600.css";
 
-import theme from '../styles/theme';
-import { MediaContextProvider } from '../media';
+import theme from "../styles/theme";
+import { MediaContextProvider } from "../media";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Head>
-				<title>Michel Gordon</title>
-			</Head>
-			<ChakraProvider theme={theme}>
-				<MediaContextProvider>
-					<Component {...pageProps} />
-				</MediaContextProvider>
-			</ChakraProvider>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Michel Gordon</title>
+        <meta
+          name="facebook-domain-verification"
+          content="lswc96loa820g6vs4d0n5vhj5onz86"
+        />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <MediaContextProvider>
+          <Component {...pageProps} />
+        </MediaContextProvider>
+      </ChakraProvider>
+    </>
+  );
 }
 export default MyApp;
